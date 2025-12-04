2bd9ac7e69263fc669f468dcdc624202f908b9fc
Eszter Éva Havasi <havasiesztereva@gmail.com>
2025-12-04 14:01:35 +0100

    Add king safety checks to King moves and captures

--- src/hu/eszter/chess/King.java
+++ src/hu/eszter/chess/King.java
@@ -31,6 +31,13 @@ public class King extends Piece {
                 }
             }
         }
+                    int[] newSquare = new int[]{newRow, newCol};
+                    if (!isProtected(board, newSquare)) {
+                        moves.add(new int[]{newRow, newCol});
+                    }
+                }
+            }
+        }
         return moves;
     }
 
@@ -79,12 +86,203 @@ public class King extends Piece {
                 }
             }
         }
+                        !(board[newRow][newCol].color).equals(this.color)) {
+                    int[] newSquare = new int[]{newRow, newCol};
+                    if (!isProtected(board, newSquare)) {
+                        moves.add(new int[]{newRow, newCol});
+                    }
+                }
+            }
+        }
         return moves;
     }
 
     @Override
     public String toString() {
         return this.color.equals("white") ? "WhiteKing" : "BlackKing";
+    }
+
+    public boolean isProtected(Piece[][] board, int[] square) {
+
+        int x = square[0];
+        int y = square[1];
+
+        // Up
+        int currentX = x - 1;
+        while (currentX >= 0) {
+            Piece p = board[currentX][y];
+            if (p == null) {
+                currentX--;
+                continue;
+            } else if (currentX == x - 1 && !((p.color).equals(this.color)) && (p instanceof Queen || p instanceof Rook || p instanceof King)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Down
+        currentX = x + 1;
+        while (currentX < board.length) {
+            Piece p = board[currentX][y];
+            if (p == null) {
+                currentX++;
+                continue;
+            } else if (currentX == x + 1 && !(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook || p instanceof King)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Left
+        int currentY = y - 1;
+        while (currentY >= 0) {
+            Piece p = board[x][currentY];
+            if (p == null) {
+                currentY--;
+                continue;
+            } else if (currentY == y + 1 && !(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook || p instanceof King)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Right
+        currentY = y + 1;
+        while (currentY < board[0].length) {
+            Piece p = board[x][currentY];
+            if (p == null) {
+                currentY++;
+                continue;
+            } else if (currentY == y + 1 && !(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook || p instanceof King)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!(p.color).equals(this.color) && (p instanceof Queen || p instanceof Rook)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Up-left
+        currentX = x-1;
+        currentY = y-1;
+        while (currentX >= 0 && currentY >= 0) {
+            Piece p = board[currentX][currentY];
+            if (p == null) {
+                currentX--;
+                currentY--;
+                continue;
+            }
+            if (currentX == x - 1 && currentY == y - 1 && this.color.equals("white") && !((p.color).equals(this.color)) && (p instanceof Pawn)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!((p.color).equals(this.color)) && (p instanceof Queen || p instanceof Bishop)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Down-right
+        currentX = x + 1;
+        currentY = y + 1;
+        while (currentX < board.length && currentY < board[0].length) {
+            Piece p = board[currentX][currentY];
+            if (p == null) {
+                currentX++;
+                currentY++;
+                continue;
+            }
+            if (currentX == x + 1 && currentY == y + 1 && this.color.equals("black") && !((p.color).equals(this.color)) && (p instanceof Pawn)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!((p.color).equals(this.color)) && (p instanceof Queen || p instanceof Bishop)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Up-right
+        currentX = x - 1;
+        currentY = y + 1;
+        while (currentX >= 0 && currentY < board[0].length) {
+            Piece p = board[currentX][currentY];
+            if (p == null) {
+                currentX--;
+                currentY++;
+                continue;
+            }
+            if (currentX == x - 1 && currentY == y + 1 && this.color.equals("white") && !((p.color).equals(this.color)) && (p instanceof Pawn)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!((p.color).equals(this.color)) && (p instanceof Queen || p instanceof Bishop)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Down-left
+        currentX = x + 1;
+        currentY = y - 1;
+        while (currentX < board.length && currentY >= 0) {
+            Piece p = board[currentX][currentY];
+            if ( p == null) {
+                currentX++;
+                currentY--;
+                continue;
+            }
+            if (currentX == x + 1 && currentY == y - 1 && this.color.equals("black") && !((p.color).equals(this.color)) && (p instanceof Pawn)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else if (!((p.color).equals(this.color)) && (p instanceof Queen || p instanceof Bishop)) {
+                System.out.println("isProtected() --> true");
+                return true;
+            } else {
+                break;
+            }
+        }
+
+        // Knight distance
+        int[] xValues = {-2, -1, 1, 2};
+        int[] yValues = {-2, -1, 1, 2};
+        for (int v : xValues) {
+            for (int w : yValues) {
+                if (Math.abs(v) == Math.abs(w)) {
+                    continue;
+                }
+                Piece p = board[x + v][y + w];
+                if (p == null) {
+                    continue;
+                }
+                if (!p.color.equals(this.color) && p instanceof Knight) {
+                    System.out.println("isProtected() --> true");
+                    return true;
+                }
+            }
+        }
+        System.out.println("isProtected() --> false");
+        return false;
     }
 
 }

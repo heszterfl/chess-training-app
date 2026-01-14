a04121c902e6b66d2ba01186101b72311ec5e3cd
Eszter Éva Havasi <havasiesztereva@gmail.com>
2026-01-14 11:33:15 +0100

    Add PieceFactory and PieceKind used by UI setup mode

--- src/hu/eszter/chess/PieceFactory.java
+++ src/hu/eszter/chess/PieceFactory.java
@@ -1,4 +1,18 @@
 package hu.eszter.chess;
 
 public class PieceFactory {
+
+    public static Piece createPiece(PieceKind kind, String color, Position position) {
+        int row = position.row();
+        int col = position.col();
+
+        return switch (kind) {
+            case PAWN -> new Pawn(color, col);
+            case ROOK -> new Rook(color, col < 4 ? "queen" : "king");
+            case KNIGHT -> new Knight(color, col < 4 ? "queen": "king");
+            case BISHOP -> new Bishop(color, col < 4 ? "queen" : "king");
+            case QUEEN -> new Queen(color);
+            case KING -> new King(color);
+        };
+    }
 }
--- src/hu/eszter/chess/PieceKind.java
+++ src/hu/eszter/chess/PieceKind.java
@@ -1,4 +1,5 @@
 package hu.eszter.chess;
 
 public enum PieceKind {
+    KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN
 }
